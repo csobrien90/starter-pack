6ecbc5afdf97b219d856312a931cc099193bce04
Chad O'Brien <obrien.music@gmail.com>
2022-03-20 23:37:17 -0400

    update security function doc

--- documentation.php
+++ documentation.php
@@ -13,4 +13,18 @@
             </li>
         </ul>
     </article>
+    <article class="documentation-function-group">
+        <h3>Security functions</h3>
+        <ul class="documentation-function-group-list">
+            <li class="documentation-function">
+                <h4>force_subscriber_role_on_registration()</h4>
+                <p><em>Force all new users to register as the 'subscriber' role.</em></p>
+                <p>To create a new admin account, an existing administrator must manually 
+                    change new user's role. This will prevent poorly secured plugins and 
+                    third-party libraries from opening your site's administrative capabilities 
+                    to programmatically created users.
+                </p>
+            </li>
+        </ul>
+    </article>
 </section>
