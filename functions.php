b7781c6033b84c8d0a9e59253a552146aef0423c
Chad O'Brien <cobrien@ket.org>
2022-03-24 18:14:27 -0500

    add recursive array search function

--- functions.php
+++ functions.php
@@ -3,3 +3,15 @@
 require_once 'functions/enqueues.php';
 require_once 'functions/utilites.php';
 require_once 'functions/security.php';
+
+$arr = array(
+	'a'	=> 'b',
+	'c'	=> array(
+		'd'	=> 'e',
+		'f'	=> 'g',
+		'h'	=> array(
+			'i'	=> 'test'
+		)
+	)
+);
+debug(find_in_array('test', $arr));
--- functions/utilites.php
+++ functions/utilites.php
@@ -5,3 +5,24 @@ function debug( $var, $die = false ) {
     echo '<pre>', print_r($var), '</pre>';
     if( $die ) die();
 }
+
+function find_in_array( $search_term, $array ) {
+
+    function recursive_find( $search_term, $array ) {
+        if ( ! is_array( $array ) ) return false;
+        foreach ( $array as $key => $value ) {
+            if ( $value == $search_term ) {
+                return $key;
+            } else if ( is_array( $value ) ) {
+                $key_result = recursive_find( $search_term, $value );
+                if ( $key_result !== false ) {
+                    return $key . ' => ' . $key_result;
+                }
+            }
+        }
+        return false;
+    }
+
+    $result = recursive_find( $search_term, $array );
+    return $result . " => $search_term";
+}
