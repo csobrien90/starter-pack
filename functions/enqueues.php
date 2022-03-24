848ef987f24382fb6fdd3485bb73b47d569b2b35
Chad O'Brien <cobrien@ket.org>
2022-03-24 17:44:52 -0500

    enqueue most recent jquery

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -4,6 +4,9 @@ function starter_pack_enqueue_scripts() {
     wp_enqueue_script( 'starter_pack_js', plugin_dir_url( __FILE__ ).'assets/js/starter-pack-scripts.js', array(), false, true );
     wp_enqueue_style( 'starter_pack_styles', plugin_dir_url( __FILE__ ).'assets/css/starter-pack-styles.css');
     wp_enqueue_style( 'dashicons' );
+
+    wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', array(), null, true);
 }
 add_action( 'wp_enqueue_scripts', 'starter_pack_enqueue_scripts' );
 add_action( 'admin_enqueue_scripts', 'starter_pack_enqueue_scripts' );
