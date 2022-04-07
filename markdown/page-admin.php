7bfa7dde66d17fb12156f04f46feb7d3b64d538c
Chad OBrien <obrien.music@gmail.com>
2022-04-06 22:46:46 -0400

    add markdown for admin page navigation

--- markdown/page-admin.php
+++ markdown/page-admin.php
@@ -3,23 +3,33 @@
     <p><em>Jumpstart devving on any WordPress site with these starter functions.</em></p>
     <hr>
 </header>
-
-<section id="utilities" class="sp-settings-panel">
-    <?php require_once 'utilities.php'; ?>
-</section>
-
-<section id="security" class="sp-settings-panel">
-    <?php require_once 'security.php'; ?>
-</section>
-
-<section id="enqueues" class="sp-settings-panel">
-    <?php require_once 'enqueues.php'; ?>
-</section>
-
-<section id="mime-types" class="sp-settings-panel">
-    <?php require_once 'mime-types.php'; ?>
-</section>
-
-<section id="code-snippets" class="sp-settings-panel">
-    <?php require_once 'code-snippets.php'; ?>
-</section>
+<main>
+    <nav>
+        <a class="btn btn-secondary">Utilities</a>
+        <a class="btn btn-secondary">Security</a>
+        <a class="btn btn-secondary">Enqueues</a>
+        <a class="btn btn-secondary">Allowed Mime Types</a>
+        <a class="btn btn-secondary">Common Code Snippets</a>
+    </nav>
+    <div class="admin-content-wrapper">
+        <section id="utilities" class="sp-settings-panel">
+            <?php require_once 'utilities.php'; ?>
+        </section>
+        
+        <section id="security" class="sp-settings-panel">
+            <?php require_once 'security.php'; ?>
+        </section>
+        
+        <section id="enqueues" class="sp-settings-panel">
+            <?php require_once 'enqueues.php'; ?>
+        </section>
+        
+        <section id="mime-types" class="sp-settings-panel">
+            <?php require_once 'mime-types.php'; ?>
+        </section>
+        
+        <section id="code-snippets" class="sp-settings-panel">
+            <?php require_once 'code-snippets.php'; ?>
+        </section>
+    </div>
+</main>
