5abf946cf177e49b21a26a68ab4f46e98e87e54b
Chad OBrien <obrien.music@gmail.com>
2022-04-30 22:00:26 -0400

    note code snippets page as under construction

--- content/code-snippets.php
+++ content/code-snippets.php
@@ -1,3 +1,9 @@
 <article class="snippets">
     <h2>Code Snippets</h2>
+    <p>This feature coming soon!</p>
+    <p>
+        <span class="dashicons dashicons-admin-tools"></span>
+        <span class="dashicons dashicons-hammer"></span>
+        <span class="dashicons dashicons-hourglass"></span>
+    </p>
 </article>
