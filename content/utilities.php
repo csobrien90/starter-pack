d6e2aa9ad961c3abedbf501004130786d3842765
Chad OBrien <obrien.music@gmail.com>
2022-06-05 00:54:49 -0400

    finesse admin page css

--- content/utilities.php
+++ content/utilities.php
@@ -5,34 +5,30 @@
 		and running with essential built-in security features and a preloaded list of common 
 		external libraries to enqueue. For the source code, installation instructions, a full list of feature, 
 		a detailed changelog, and to request features or report any issues/bugs, please visit the 
-		<a href="https://github.com/csobrien90/starter-pack" target="_blank">
-			Starter Pack Plugin on GitHub
-		</a>.
+		<a href="https://github.com/csobrien90/starter-pack" target="_blank">Starter Pack Plugin on GitHub</a>.
 	</p>
 	<p>This plugin was authored by Chad O'Brien, a web developer based in Louisville, Kentucky. 
 		It was created and is maintained within the principles of 
-		<a href="https://opensource.org/osd" target="_blank">
-			the Open Source Definition
-		</a>.
+		<a href="https://opensource.org/osd" target="_blank">the Open Source Definition</a>.
 	</p>
 </article>
 <article class="utility-functions">
 	<h2>Utility functions</h2>
 	<ul class="utility-functions-list">
 		<li class="utility-function">
-			<h4>debug( $var, $die = false )</h4>
+			<h3>debug( $var, $die = false )</h3>
 			<p><em>Print $var, deciding whether to die after</em></p>
 		</li>
 		<li class="utility-function">
-			<h4>log_this( $var )</h4>
+			<h3>log_this( $var )</h3>
 			<p><em>Log $var to the console as a string or object</em></p>
 		</li>
 		<li class="utility-function">
-			<h4>php_to_js( $var, $new_var_name = 'a', $new_var_type = 'var' )</h4>
+			<h3>php_to_js( $var, $new_var_name = 'a', $new_var_type = 'var' )</h3>
 			<p><em>Convert a php variable ($var) to JavaScript - specify the name of the created variable ($new_var_name) and whether to declare it with let, var, or const ($new_var_type)</em></p>
 		</li>
 		<li class="utility-function">
-			<h4>deep_search( $search_term, $array_or_object )</h4>
+			<h3>deep_search( $search_term, $array_or_object )</h3>
 			<p><em>Find first instance of $search_term in any combination of nested arrays and object, then return the path</em></p>
 		</li>
 	</ul>
