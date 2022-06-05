23785725cc28583641513c3def4781070f685a70
Chad OBrien <obrien.music@gmail.com>
2022-06-05 00:04:51 -0400

    update about this plugin copy on admin page

--- content/utilities.php
+++ content/utilities.php
@@ -1,26 +1,39 @@
 <article class="about-this-plugin">
-    <h2>About this plugin</h2>
-    <p>Plugin description and features</p>
-    <p>Repo links and credits</p>
+	<h2>About this plugin</h2>
+	<p>Starter Pack is designed to be the first plugin you install on any WordPress site. It 
+		provides tools to help developers work on custom plugins/themes and gets any site up 
+		and running with essential built-in security features and a preloaded list of common 
+		external libraries to enqueue. For the source code, installation instructions, a full list of feature, 
+		a detailed changelog, and to request features or report any issues/bugs, please visit the 
+		<a href="https://github.com/csobrien90/starter-pack" target="_blank">
+			Starter Pack Plugin on GitHub
+		</a>.
+	</p>
+	<p>This plugin was authored by Chad O'Brien, a web developer based in Louisville, Kentucky. 
+		It was created and is maintained within the principles of 
+		<a href="https://opensource.org/osd" target="_blank">
+			the Open Source Definition
+		</a>.
+	</p>
 </article>
 <article class="utility-functions">
-    <h2>Utility functions</h2>
-    <ul class="utility-functions-list">
-        <li class="utility-function">
-            <h4>debug( $var, $die = false )</h4>
-            <p><em>Print $var, deciding whether to die after</em></p>
-        </li>
-        <li class="utility-function">
-            <h4>log_this( $var )</h4>
-            <p><em>Log $var to the console as a string or object</em></p>
-        </li>
-        <li class="utility-function">
-            <h4>php_to_js( $var, $new_var_name = 'a', $new_var_type = 'var' )</h4>
-            <p><em>Convert a php variable ($var) to JavaScript - specify the name of the created variable ($new_var_name) and whether to declare it with let, var, or const ($new_var_type)</em></p>
-        </li>
-        <li class="utility-function">
-            <h4>deep_search( $search_term, $array_or_object )</h4>
-            <p><em>Find first instance of $search_term in any combination of nested arrays and object, then return the path</em></p>
-        </li>
-    </ul>
+	<h2>Utility functions</h2>
+	<ul class="utility-functions-list">
+		<li class="utility-function">
+			<h4>debug( $var, $die = false )</h4>
+			<p><em>Print $var, deciding whether to die after</em></p>
+		</li>
+		<li class="utility-function">
+			<h4>log_this( $var )</h4>
+			<p><em>Log $var to the console as a string or object</em></p>
+		</li>
+		<li class="utility-function">
+			<h4>php_to_js( $var, $new_var_name = 'a', $new_var_type = 'var' )</h4>
+			<p><em>Convert a php variable ($var) to JavaScript - specify the name of the created variable ($new_var_name) and whether to declare it with let, var, or const ($new_var_type)</em></p>
+		</li>
+		<li class="utility-function">
+			<h4>deep_search( $search_term, $array_or_object )</h4>
+			<p><em>Find first instance of $search_term in any combination of nested arrays and object, then return the path</em></p>
+		</li>
+	</ul>
 </article>
